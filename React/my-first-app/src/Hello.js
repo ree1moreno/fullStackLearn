9dd3518368a14061b6fb22343a1d9d5ecf3bee66
Renato Moreno <renats.mc@gmail.com>
2022-09-01 17:40:34 -0300

    criando primeiro componente

---
+++ React/my-first-app/src/Hello.js
@@ -0,0 +1,15 @@
+// import { Component } from "react";
+
+export default function HelloWorld() {
+  return <h1>Hello World</h1>;
+}
+
+// export const HelloWorld = () => {
+//   return <h1>Hello World</h1>;
+// };
+
+// export default class HelloWorld extends Component {
+//   render() {
+//     return <h1>Hello World</h1>;
+//   }
+// }
