b8349fd56fb1a0902f60e75bf1d08ff5ff1aba6d
Renato Moreno <renats.mc@gmail.com>
2022-09-03 14:37:28 -0300

    conteudo de componentes react

--- React/my-first-app/src/Components/planets/planet/index.js
+++ React/my-first-app/src/Components/planets/planet/index.js
@@ -1,11 +1,30 @@
 import GrayImg from "../../shared/gray_img";
 
 const Planet = (props) => {
+  const names = ["A", "B", "C", "D"];
+  // const satellites = names.map((n, i) => <li key={i}>Satélite {n}</li>);
+
+  if (!props.description)
+    return (
+      <div onClick={() => props.clickOnPlanet(props.name)}>
+        <h4>{props.name}</h4>
+        <GrayImg img_url={props.img_url} />
+      </div>
+    );
+
   return (
     <div onClick={() => props.clickOnPlanet(props.name)}>
       <h4>{props.name}</h4>
       <p>{props.description}</p>
       <GrayImg img_url={props.img_url} />
+      <h3>Satélites</h3>
+      {/* <ul>{satellites}</ul> */}
+      <ul>
+        {names.map((n, i) => (
+          <li key={i}>Satélite {n}</li>
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 };
