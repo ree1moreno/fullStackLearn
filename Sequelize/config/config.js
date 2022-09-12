8ed03fb8c92d3d6708fd26842b7d41de298007ef
Renato Moreno <renats.mc@gmail.com>
2022-09-12 17:21:30 -0300

    configuracao inicial sequelize + criacao banco de dados

---
+++ Sequelize/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "teste-senha",
+  database: "curso_sequelize",
+  define: {
+    timestamps: true,
+  },
+};
