902a467f91a05faf2abadeff4c4490a0b8b2c184
Renato Moreno <renats.mc@gmail.com>
2022-09-12 17:38:57 -0300

    criando primeira migration e tabela planets no banco de dados

---
+++ Sequelize/migrations/20220912202621-planets.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("planets", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      position: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("planets");
+  },
+};
