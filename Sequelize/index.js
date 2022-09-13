c5f7d9dba0665dbf51fe37bae1fe986d9ab9bb09
Renato Moreno <renats.mc@gmail.com>
2022-09-12 21:50:30 -0300

    fazendo consultas no banco de dados

--- Sequelize/index.js
+++ Sequelize/index.js
@@ -1,10 +1,20 @@
 (async () => {
   const Planet = require("./models/Planet");
 
+  /*
   const newPlanet = await Planet.create({
     name: "Vênus",
     position: 5,
   });
+  */
 
-  console.log(newPlanet);
+  // const seePlanets = await Planet.findAll();
+  // const seePlanets = await Planet.findByPk(2);
+  const seePlanets = await Planet.findAll({
+    where: {
+      name: "Terra",
+    },
+  });
+
+  console.log(seePlanets);
 })();
