128fd0fcb60c352f894f2897ff6502a74e76f034
Renato Moreno <renats.mc@gmail.com>
2022-10-12 14:30:18 -0300

    finalizando modulo

--- Javascript/moderno-atualizado/usando_o_webpack/webpack.config.js
+++ Javascript/moderno-atualizado/usando_o_webpack/webpack.config.js
@@ -1,14 +1,32 @@
-const path = require("path");
+// const path = require("path");
+
+// module.exports = {
+//   entry: {
+//     main: "./src/index.js",
+//     hello: "./src/hello.js",
+//   },
+//   // mode: "development",
+//   mode: "production",
+//   output: {
+//     path: path.resolve(__dirname, "public"),
+//     filename: "[name].bundle.min.js",
+//   },
+// };
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    main: "./src/index.js",
-    hello: "./src/hello.js",
+    index: "./src/index.js",
   },
-  // mode: "development",
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "[name].bundle.min.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
