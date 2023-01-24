a1a86c1cc583efb0dc41ef23b4347c9055180e2a
Renato Moreno <renats.mc@gmail.com>
2023-01-24 16:52:31 -0300

    criando terceiro resource: Episodes

--- oneBitFlix/onebitflix/src/models/index.ts
+++ oneBitFlix/onebitflix/src/models/index.ts
@@ -1,8 +1,12 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
 Category.hasMany(Course);
 
 Course.belongsTo(Category);
+Course.hasMany(Episode);
 
-export { Category, Course };
+Episode.belongsTo(Course);
+
+export { Category, Course, Episode };
